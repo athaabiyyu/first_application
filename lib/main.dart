1c5b98975b8c9e20adedf2c04326e6b5e787ee86
Aulia Atha <auliaatha123@gmail.com>
2025-02-04 13:40:10 +0700

    Row & Coloumn

--- lib/main.dart
+++ lib/main.dart
@@ -17,21 +17,36 @@ class MyApp extends StatelessWidget {
             ),
           ),
         ),
-        body: Center(
-          child: Container(
-            color: Colors.lightBlue,
-            width: 150,
-            height: 50,
-            child: Text(
-              "Hello, World!",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.white,
-                fontStyle: FontStyle.italic,
-                fontWeight: FontWeight.w700,
-              ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: Container(
+                color: Colors.lightBlue,
+                width: 150,
+                height: 50,
+                child: Text(
+                  "Hello, World!",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontStyle: FontStyle.italic,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+              )
             ),
-          )
+            Text("Test 1"),
+            Text("Test 2"),
+            Text("Test 3"),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Test 4"),
+                Text("Test 5"),
+              ],
+            )
+          ],
         ),
       ),
     );
